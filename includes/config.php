2c77158e1419590aee6febf387f227d2d607b0b3
Matan Omesi <matanfxp@hotmail.co.il>
2020-11-24 08:32:10 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$_Data['Host'] = 'MATAN-PC';//'46.4.71.92';//'MATAN1346\SQLEXPRESS';//93.115.94.120
+$_Data['Host'] = 'PC';
 $_Data['User'] = 'sa';
-$_Data['Pass'] = '84269713n,i';//'w42msdWsdQs231';//'84269713n,i';
-$_Data['Base'] = 'GunzDB3';//'GunzDB3';
+$_Data['Pass'] = 'password';
+$_Data['Base'] = 'dbname';
